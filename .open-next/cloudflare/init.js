d7958944aaa5e58fdc6d4037e89a91fe67a850ad
sebelobrov <se.belobrov@gmail.com>
2025-08-14 23:57:27 +0300

    fix2

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1755204507706,
+    __BUILD_TIMESTAMP_MS__: 1755204992068,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     // The external middleware will use the convertTo function of the `edge` converter
